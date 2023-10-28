12a77a25dccb0cbdc79d4e6627f094fbc590afe2
Jose-Alisson <84239112+Jose-Alisson@users.noreply.github.com>
2023-10-28 14:00:19 -0300

    outubro rosa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,59 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'outubro-rosa';
 
-  @ViewChild('initialText', {static: true})
-  initialText!: ElementRef<HTMLElement>
+  @ViewChild('initialText', { static: true })
+  initialText!: ElementRef<HTMLElement>;
 
-  saibaMais(){
-    this.initialText.nativeElement.scrollIntoView({behavior: 'smooth', inline: 'center'})
+  saibaMais() {
+    let bounds = this.initialText.nativeElement.getBoundingClientRect()
+
+    window.scroll({left: 0, top: bounds.top, behavior: 'smooth'})
+
+    // this.initialText.nativeElement.scrollIntoView({
+    //   behavior: 'smooth',
+    //   inline: 'center',
+    // });
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  scoll(event: MouseEvent) {
+    let y = window.innerHeight;
+
+    document.querySelectorAll('.info .text').forEach((element) => {
+      let bounds = element.getBoundingClientRect();
+      let yElement = bounds.y;
+
+      if (bounds.top + bounds.height / 2 < y) {
+        element.classList.remove('fade');
+      } else {
+        element.classList.add('fade');
+      }
+    });
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  scollTab(event: KeyboardEvent) {
+    let y = window.innerHeight;
+    let current = -1;
+
+    if (event.key === 'ArrowDown') {
+      document.querySelectorAll('.info').forEach((element, index) => {
+
+        let bounds = element.getBoundingClientRect();
+
+        if (element.classList.contains('fade')) {
+            window.scroll({top: (bounds.bottom) , behavior : 'smooth'})
+        }
+      });
+
+      current = -1
+    }
   }
 }
